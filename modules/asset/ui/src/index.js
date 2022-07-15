de997c5ce79ca499bedf1d5e2e0567ab8b312a81
ramcd86 <ra_mcd@yahoo.com>
2022-07-15 12:51:10 +0200

    Tweaks

--- modules/asset/ui/src/index.js
+++ modules/asset/ui/src/index.js
@@ -1,11 +1,30 @@
 export default () => {
-  const urlSegments = window.location.pathname.split("/");
-  const pageBase = document.getElementById("pageBase");
-  if (pageBase && urlSegments) {
-    if (urlSegments[1] === "") {
-      pageBase.classList.add("home");
-    } else {
-      pageBase.classList.add(`${urlSegments[1]}`);
+
+  const handlePageStyling = () => {
+    const urlSegments = window.location.pathname.split("/");
+    const pageBase = document.getElementById("pageBase");
+    if (pageBase && urlSegments) {
+      if (urlSegments[1] === "") {
+        pageBase.classList.add("home");
+      } else {
+        pageBase.classList.add(`${urlSegments[1]}`);
+      }
     }
-  }
+  };
+
+  const handleSubnavLinkHighlighting = () => {
+    const subNavLinks = document.querySelectorAll('.subnav-items');
+    if (subNavLinks && subNavLinks.length > 0) {
+      subNavLinks.forEach(navLink => {
+        console.log(navLink.href);
+        if (navLink.href === window.location.href) {
+          navLink.classList.add('selected-subnav-item');
+        }
+      });
+    }
+  };
+
+  handlePageStyling();
+  handleSubnavLinkHighlighting();
+
 };
