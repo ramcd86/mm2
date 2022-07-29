2719d1aa65ee6df87d9b2aeb2ca2f7a0d230815d
ramcd86 <ra_mcd@yahoo.com>
2022-07-29 14:58:42 +0200

    Tweaks

--- modules/@apostrophecms/page/index.js
+++ modules/@apostrophecms/page/index.js
@@ -5,81 +5,85 @@ module.exports = {
   options: {
     types: [
       {
-        name: "default-page",
-        label: "Default",
+        name: '@apostrophecms/search',
+        label: 'Search'
       },
       {
-        name: "@apostrophecms/home-page",
-        label: "Home",
+        name: 'default-page',
+        label: 'Default'
       },
       {
-        name: "organdir-page",
-        label: "Organ Directory",
+        name: '@apostrophecms/home-page',
+        label: 'Home'
       },
       {
-        name: "organdir-alphabase-page",
-        label: "Organs Alphabetical Base Page",
+        name: 'organdir-page',
+        label: 'Organ Directory'
       },
       {
-        name: "builderdir-page",
-        label: "Organ Builder Directory",
+        name: 'organdir-alphabase-page',
+        label: 'Organs Alphabetical Base Page'
       },
       {
-        name: "builderdir-alphabase-page",
-        label: "Organ Builders Alphabetical Base Page",
+        name: 'builderdir-page',
+        label: 'Organ Builder Directory'
       },
       {
-        name: "musiciandir-alphabase-page",
-        label: "Church Musicians Alphabetical Base Page",
+        name: 'builderdir-alphabase-page',
+        label: 'Organ Builders Alphabetical Base Page'
       },
       {
-        name: "musiciandir-page",
-        label: "Church Musicians Directory",
+        name: 'musiciandir-alphabase-page',
+        label: 'Church Musicians Alphabetical Base Page'
       },
       {
-        name: "queens-portal",
-        label: "Queen's Portal",
+        name: 'musiciandir-page',
+        label: 'Church Musicians Directory'
       },
       {
-        name: "queens-default-page",
-        label: "Queen's Portal Standard Page",
+        name: 'queens-portal',
+        label: 'Queen\'s Portal'
       },
       {
-        name: "queensnews-page",
-        label: "Queen's News Page",
+        name: 'queens-default-page',
+        label: 'Queen\'s Portal Standard Page'
       },
       {
-        name: "shop-portal",
-        label: "Shop Portal",
+        name: 'queensnews-page',
+        label: 'Queen\'s News Page'
       },
       {
-        name: "publisher-portal",
-        label: "Publisher (Book & Note) Portal",
+        name: 'shop-portal',
+        label: 'Shop Portal'
       },
       {
-        name: "publisher-default-page",
-        label: "Publisher's Standard Page",
+        name: 'publisher-portal',
+        label: 'Publisher (Book & Note) Portal'
       },
       {
-        name: "publisher-delivery-portal",
-        label: "Publisher Delivery (KDL-VA)",
+        name: 'publisher-default-page',
+        label: 'Publisher\'s Standard Page'
       },
       {
-        name: "publicationsdir-alphabase-page",
-        label: "Publications Alphabetical Base Page",
+        name: 'publisher-delivery-portal',
+        label: 'Publisher Delivery (KDL-VA)'
       },
       {
-        name: "publicationsdir-page",
-        label: "Publications Directory",
+        name: 'publicationsdir-alphabase-page',
+        label: 'Publications Alphabetical Base Page'
       },
       {
-        name: "notenkeller-portal",
-        label: "Notenkeller Celle Portal",
+        name: 'publicationsdir-page',
+        label: 'Publications Directory'
       },
       {
-        name: "church-music-design-portal",
-        label: "Church Music Design (KMD)",
+        name: 'notenkeller-portal',
+        label: 'Notenkeller Celle Portal'
       },
-    ],
-  },
+      {
+        name: 'church-music-design-portal',
+        label: 'Church Music Design (KMD)'
+      }
+    ]
+  }
 };
