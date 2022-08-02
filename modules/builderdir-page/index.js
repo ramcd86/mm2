4d922bf38fb9c69baf1be527a74066ff01f59ead
ramcd86 <ra_mcd@yahoo.com>
2022-08-02 14:08:59 +0200

    Tweaks

--- modules/builderdir-page/index.js
+++ modules/builderdir-page/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extend: "@apostrophecms/piece-page-type",
+  extend: '@apostrophecms/piece-page-type',
   methods: (self, options) => {
     self.buildUrl = (req, page, piece) => {
       if (!page) {
@@ -7,7 +7,7 @@ module.exports = {
       }
       return piece.externalUrl
         ? piece.externalUrl
-        : page._url + "/" + piece.slug;
+        : page._url + '/' + piece.slug;
     };
     self.chooseParentPage = (pages, piece) => {
       return pages.find((page) => page.alpha === piece.alpha);
@@ -17,189 +17,178 @@ module.exports = {
     };
   },
   options: {
-    label: "Organ Builders Directory Page",
-    pluralLabel: "Organ Builders Directory Pages",
+    label: 'Organ Builders Directory Page',
+    pluralLabel: 'Organ Builders Directory Pages'
   },
   fields: {
     add: {
       subtitle: {
-        type: "string",
-        label: "Page Header",
+        type: 'string',
+        label: 'Page Header',
         textarea: false,
-        required: true,
+        required: true
       },
       alpha: {
-        type: "select",
-        label: "Organs Builders by Alphabet",
+        type: 'select',
+        label: 'Organs Builders by Alphabet',
         required: true,
         readOnly: false,
         choices: [
           {
-            label: "A",
-            value: "A",
+            label: 'A',
+            value: 'A'
           },
           {
-            label: "B",
-            value: "B",
+            label: 'B',
+            value: 'B'
           },
           {
-            label: "C",
-            value: "C",
+            label: 'C',
+            value: 'C'
           },
           {
-            label: "D",
-            value: "D",
+            label: 'D',
+            value: 'D'
           },
           {
-            label: "E",
-            value: "E",
+            label: 'E',
+            value: 'E'
           },
           {
-            label: "F",
-            value: "F",
+            label: 'F',
+            value: 'F'
           },
           {
-            label: "G",
-            value: "G",
+            label: 'G',
+            value: 'G'
           },
           {
-            label: "H",
-            value: "H",
+            label: 'H',
+            value: 'H'
           },
           {
-            label: "I",
-            value: "I",
+            label: 'I',
+            value: 'I'
           },
           {
-            label: "J",
-            value: "J",
+            label: 'J',
+            value: 'J'
           },
           {
-            label: "K",
-            value: "K",
+            label: 'K',
+            value: 'K'
           },
           {
-            label: "L",
-            value: "L",
+            label: 'L',
+            value: 'L'
           },
           {
-            label: "M",
-            value: "M",
+            label: 'M',
+            value: 'M'
           },
           {
-            label: "N",
-            value: "N",
+            label: 'N',
+            value: 'N'
           },
           {
-            label: "O",
-            value: "O",
+            label: 'O',
+            value: 'O'
           },
           {
-            label: "P",
-            value: "P",
+            label: 'P',
+            value: 'P'
           },
           {
-            label: "Q",
-            value: "Q",
+            label: 'Q',
+            value: 'Q'
           },
           {
-            label: "R",
-            value: "R",
+            label: 'R',
+            value: 'R'
           },
           {
-            label: "S",
-            value: "S",
+            label: 'S',
+            value: 'S'
           },
           {
-            label: "T",
-            value: "T",
+            label: 'T',
+            value: 'T'
           },
           {
-            label: "U",
-            value: "U",
+            label: 'U',
+            value: 'U'
           },
           {
-            label: "V",
-            value: "V",
+            label: 'V',
+            value: 'V'
           },
           {
-            label: "W",
-            value: "W",
+            label: 'W',
+            value: 'W'
           },
           {
-            label: "X",
-            value: "X",
+            label: 'X',
+            value: 'X'
           },
           {
-            label: "Y",
-            value: "Y",
+            label: 'Y',
+            value: 'Y'
           },
           {
-            label: "Z",
-            value: "Z",
-          },
-        ],
+            label: 'Z',
+            value: 'Z'
+          }
+        ]
       },
       main: {
-        type: "area",
+        type: 'area',
         options: {
           widgets: {
-            "custom-image": {},
-            "@apostrophecms/rich-text": {
+            'custom-image': {},
+            '@apostrophecms/rich-text': {
               toolbar: [
-                "styles",
-                "|",
-                "bold",
-                "italic",
-                "strike",
-                "link",
-                "|",
-                "blockquote",
-                "bulletList",
-                "orderedList",
-                "alignLeft",
-                "alignCenter",
-                "alignRight",
-                "undo",
-                "redo",
+                'styles',
+                '|',
+                'bold',
+                'italic',
+                'strike',
+                'link',
+                '|',
+                'blockquote',
+                'bulletList',
+                'orderedList',
+                'alignLeft',
+                'alignCenter',
+                'alignRight',
+                'undo',
+                'redo'
               ],
               styles: [
                 {
-                  tag: "p",
-                  label: "Paragraph (P)",
+                  tag: 'p',
+                  label: 'Paragraph (P)'
                 },
                 {
-                  tag: "h3",
-                  label: "Heading 3 (H3)",
+                  tag: 'h3',
+                  label: 'Heading 3 (H3)'
                 },
                 {
-                  tag: "h4",
-                  label: "Heading 4 (H4)",
-                },
-              ],
+                  tag: 'h4',
+                  label: 'Heading 4 (H4)'
+                }
+              ]
             },
-            "@apostrophecms/html": {},
-            "custom-divider": {},
-          },
-        },
-      },
-      // _topics: {
-      //   type: "relationship",
-      //   label: "Blog post topic",
-      //   withType: "topic",
-      //   builders: {
-      //     project: {
-      //       title: 1,
-      //       _url: 1,
-      //     },
-      //   },
-      // },
+            '@apostrophecms/html': {},
+            'custom-divider': {}
+          }
+        }
+      }
     },
     group: {
       basics: {
-        label: "Basics",
-        fields: ["title", "subtitle", "main", "_topics", "alpha"],
-      },
-    },
-  },
+        label: 'Basics',
+        fields: [ 'title', 'subtitle', 'main', '_topics', 'alpha' ]
+      }
+    }
+  }
 };
